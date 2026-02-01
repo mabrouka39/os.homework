a4c3a5d36cce1d3fdb929fbd9fde45daf8b93fd0
mabrouka39 <kokarouka91@gmail.com>
2026-02-01 22:28:47 +0100

    Update kernal.cpp

--- kernal.cpp
+++ kernal.cpp
@@ -1,7 +1,10 @@
-xtern "C" void main () {
-2 // Pointer to Video Memory
-3 char * video_memory = ( char *) 0 xb8000 ;
-4
-5 // Print ’X’ in the top - left corner
-6 video_memory [0] = ’X’;
-7 video_memory [1] = 0 x0f ; // White on Black
+
+extern "C" void afficher() {
+    char* video = (char*)0xB8000;
+    video[0] = 'X';
+    video[1] = 0x0F;
+}
+
+extern "C" void main() {
+    afficher();
+}
