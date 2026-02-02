244b664a922c520c8c04757333e378b8a656b9be
mabrouka39 <kokarouka91@gmail.com>
2026-02-03 00:42:30 +0100

    Implement PrintCharacter for screen output
    Refactor display functions to use PrintCharacter for output.

--- kernal.cpp
+++ kernal.cpp
@@ -1,10 +1,32 @@
+extern "C" {
 
-extern "C" void afficher() {
-    char* video = (char*)0xB8000;
-    video[0] = 'X';
-    video[1] = 0x0F;
+unsigned short VideoEntry(char character, unsigned char color) {
+    return static_cast<unsigned short>(character) | (color << 8);
 }
 
-extern "C" void main() {
-    afficher();
+unsigned short* video = (unsigned short*)0xB8000;
+unsigned char current_line = 0;
+unsigned char current_pos = 0;
+const unsigned char VIDEO_WIDTH = 80;
+const unsigned char VIDEO_HEIGHT = 25;
+
+void PrintCharacter(char character, unsigned char color) {
+    if(character == '\n') {
+        current_line++;
+        current_pos = 0;
+    } else {
+        video[current_line * VIDEO_WIDTH + current_pos] = VideoEntry(character, color);
+        current_pos++;
+        if(current_pos == VIDEO_WIDTH) {
+            current_pos = 0;
+            current_line++;
+            if(current_line == VIDEO_HEIGHT) current_line = 0;
+        }
+    }
+}
+
+void main() {
+    PrintCharacter('X', 0x0F);
+}
+
 }
